8525fcdac7bef316e2f55eb4674e9c8e8a520acf
Duyet Mai <duyet.mai@adurolife.com>
2022-02-25 19:49:32 -0800

    fix name

--- terraform/main.tf
+++ terraform/main.tf
@@ -133,10 +133,10 @@ module "trigger_today_ooo" {
   trigger_input               = { "lambda_trigger_event" : "TODAY_OOO" }
 }
 
-module "trigger_today_ooo" {
+module "trigger_lambda_warm_up" {
   source                      = "./modules/cloud_watch_event_trigger_lambda_schedule"
-  schedule_name               = "trigger_today_ooo"
-  schedule_desc               = "trigger_today_ooo"
+  schedule_name               = "trigger_lambda_warm_up"
+  schedule_desc               = "trigger_lambda_warm_up"
   schedule_expression         = "cron(5 * * * *)"
   schedule_lambda_target_arn  = module.lambda_function.lambda_function_arn
   schedule_lambda_target_name = module.lambda_function.lambda_function_name
