be51c696b0fe4082dde530fc20814d1262de2381
Duyet Mai <duyet.mai@adurolife.com>
2022-02-26 17:22:36 -0800

    add block template handler and move all inline template to json file

--- application/handlers/bot/pto_register.py
+++ application/handlers/bot/pto_register.py
@@ -6,6 +6,7 @@ from slack_bolt import App
 from slack_sdk import WebClient
 from tenacity import retry, wait_fixed, stop_after_attempt
 
+from application.handlers.bot.block_template_handler import BlockTemplateHandler
 from application.handlers.database.google_sheet import GoogleSheetDB
 
 
@@ -27,148 +28,19 @@ class PTORegister:
         app.block_action({"block_id": "approve_deny_vacation", "action_id": "deny"})(
             ack=self.respond_to_slack_within_3_seconds,
             lazy=[self.deny_pto])
+        self.block_kit = BlockTemplateHandler('./application/handlers/bot/block_templates').get_object_templates()
 
     @staticmethod
     def respond_to_slack_within_3_seconds(ack):
         ack()
 
-    @staticmethod
-    def trigger_request_leave_command(client, body, ack):
+    def trigger_request_leave_command(self, client, body, ack):
         client.views_open(
             trigger_id=body.get('trigger_id'),
-            view={
-
-                "type": "modal",
-                "callback_id": "leave_input_view",
-
-                "title": {"type": "plain_text", "text": "Request a leave"},
-                "close": {"type": "plain_text", "text": "Close"},
-                "submit": {"type": "plain_text", "text": "Submit"},
-                "blocks": [
-
-                    {
-                        "type": "input",
-                        "block_id": "leave_type",
-
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Leave Type"
-                        },
-                        "element": {
-                            "action_id": "leave_type",
-                            "type": "static_select",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Select leave type"
-                            },
-                            "initial_option":
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":palm_tree:PTO",
-                                        "emoji": True
-
-                                    },
-                                    "value": "PTO"
-                                },
-                            "options": [
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":palm_tree:PTO",
-                                        "emoji": True
-
-                                    },
-                                    "value": "PTO"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":palm_tree:Quarter PTO",
-                                        "emoji": True
-
-                                    },
-                                    "value": "Quarter PTO"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":money_with_wings:UTO",
-                                        "emoji": True
-
-                                    },
-                                    "value": "UTO"
-                                },
-                                {
-                                    "text": {
-                                        "type": "plain_text",
-                                        "text": ":thermometer:Sick",
-                                        "emoji": True
-
-                                    },
-                                    "value": "Sick"
-                                }
-                            ]
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "optional": False,
-
-                        "block_id": "vacation_start_date",
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Start date:"
-                        },
-                        "element": {
-                            "type": "datepicker",
-                            "action_id": "vacation_start_date_picker",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Select a date"
-                            }
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "optional": False,
-                        "block_id": "vacation_end_date",
-                        "label": {
-                            "type": "plain_text",
-                            "text": "End date:"
-                        },
-                        "element": {
-                            "type": "datepicker",
-                            "action_id": "vacation_end_date_picker",
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Select a date"
-                            }
-                        }
-                    },
-                    {
-                        "type": "input",
-                        "block_id": "reason_of_leave",
-                        "label": {
-                            "type": "plain_text",
-                            "text": "Details (optional)",
-                        },
-                        "element": {
-                            "type": "plain_text_input",
-                            "action_id": "reason_for_leave",
-                            "multiline": True,
-                            "placeholder": {
-                                "type": "plain_text",
-                                "text": "Specific why you're taking the time off."
-                            }
-                        }
-                    },
-                ],
-            }
+            view=json.loads(self.block_kit.leave_input_view(callback_id='leave_input_view'))
         )
 
-    @staticmethod
-    def get_leave_confirmation_view(body, ack):
+    def get_leave_confirmation_view(self, body, ack):
 
         values = body.get("view").get("state").get("values")
 
@@ -177,65 +49,25 @@ class PTORegister:
 
         start_date_str = values.get("vacation_start_date").get("vacation_start_date_picker").get("selected_date")
         end_date_str = values.get("vacation_end_date").get("vacation_end_date_picker").get("selected_date")
-        leave_view = {
-            "callback_id": "leave_confirmation_view",
-            "private_metadata": json.dumps({
-                "reason_of_leave": reason_of_leave,
-                "leave_type": leave_type,
-                "start_date_str": start_date_str,
-                "end_date_str": end_date_str
+        private_metadata = json.dumps({
+            "reason_of_leave": reason_of_leave,
+            "leave_type": leave_type,
+            "start_date_str": start_date_str,
+            "end_date_str": end_date_str
 
-            }),
-            "type": "modal",
-            "blocks": [
-                {
-                    "type": "section",
-                    "block_id": "B/i",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"*What?*\n:palm_tree: {leave_type}",
-                        "verbatim": False
-                    }
-                },
-                {
-                    "type": "section",
-                    "block_id": "8PS",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"*When?*\n:calendar: {start_date_str} - {end_date_str}",
-                        "verbatim": False
-                    }
-                },
-                {
-                    "type": "section",
-                    "block_id": "g0W",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"*Comment*\n:speech_balloon: {reason_of_leave}",
-                        "verbatim": False
-                    }
-                }
-            ],
-            "title": {
-                "type": "plain_text",
-                "text": "Your time off",
-                "emoji": True
-            },
-            "clear_on_close": False,
-            "notify_on_close": False,
-            "close": {
-                "type": "plain_text",
-                "text": "Back",
-                "emoji": True
-            },
-            "submit": {
-                "type": "plain_text",
-                "text": "Confirm",
-                "emoji": True
-            },
-        }
+        })
+        leave_confirmation_view = self.block_kit.leave_confirmation_view(callback_id='leave_confirmation_view',
+                                                                         leave_type=leave_type,
+                                                                         start_date_str=start_date_str,
+                                                                         end_date_str=end_date_str,
+                                                                         reason_of_leave=reason_of_leave
+                                                                         )
+
+        leave_confirmation_view = json.loads(leave_confirmation_view)
+        leave_confirmation_view['private_metadata'] = private_metadata
+
         ack(response_action="push",
-            view=leave_view)
+            view=leave_confirmation_view)
 
     # Update the view on submission
     def handle_leave_request_submission(self, body, logger):
@@ -258,133 +90,32 @@ class PTORegister:
         leave_id = uuid.uuid4()
 
         self._insert_item(leave_id, leave_type, reason_of_leave, user_name, start_date_str, end_date_str, time_now)
-
+        channel_message_block = self.block_kit.new_vacation_request_channel_message_blocks(
+            user_profile_url=user_profile_url,
+            user_name=user_name,
+            reason_of_leave=reason_of_leave,
+            leave_type=leave_type,
+            leave_id=leave_id,
+            start_date=start_date,
+            end_date=end_date
+        )
         self.client.chat_postMessage(
             channel=self.approval_channel,
             text=f"You have a new request:\n*<{user_profile_url}|{user_name} - New vacation request>*",
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"You have a new request:\n*<{user_profile_url}|{user_name} - New vacation request>*"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f"*Reason:*\n{reason_of_leave}"
-                    },
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*Leave Type:*\n{leave_type}"
-                        },
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*Leave Id:*\n{leave_id}"
-                        }
-                    ]
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
+            blocks=channel_message_block)
 
-                            "type": "mrkdwn",
-                            "text": f"*Start Date:*\n{start_date.strftime('%A, %B, %d, %Y')}"
-
-                        },
-                        {
-                            "type": "mrkdwn",
-                            "text": f"*End Date:*\n{end_date.strftime('%A, %B, %d, %Y')}"
-
-                        }
-                    ]
-                },
-                {
-                    "type": "actions",
-                    "block_id": "approve_deny_vacation",
-                    "elements": [
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "emoji": True,
-                                "text": "Approve"
-                            },
-                            "action_id": "approve",
-                            "style": "primary",
-                            "value": "approve_vacation_request"
-                        },
-                        {
-                            "type": "button",
-                            "text": {
-                                "type": "plain_text",
-                                "emoji": True,
-                                "text": "Deny"
-                            },
-                            "action_id": "deny",
-                            "style": "danger",
-                            "value": "deny_vacation_request"
-                        }
-                    ]
-                }
-            ])
         logger.info(body)
+        confirm_requester_message_block = json.loads(self.block_kit.vacation_request_confirm_requester_message_blocks(
+            leave_type=leave_type,
+            leave_id=leave_id,
+            start_date=start_date,
+            end_date=end_date,
+            reason_of_leave=reason_of_leave
+        ))
         self.client.chat_postMessage(channel=user_id,
                                      text=f"You have sent a new vacation request. "
                                           f"Please wait to manager approve",
-                                     blocks=[{
-                                         "type": "section",
-                                         "text": {
-                                             "type": "mrkdwn",
-                                             "text": f"You have sent a new vacation request. "
-                                                     f"Please wait to manager approve"
-                                         }
-                                     },
-
-                                         {
-                                             "type": "section",
-                                             "fields": [
-                                                 {
-                                                     "type": "mrkdwn",
-                                                     "text": f"*Leave Type:*\n{leave_type}"
-                                                 },
-                                                 {
-                                                     "type": "mrkdwn",
-                                                     "text": f"*Leave Id:*\n{leave_id}"
-                                                 }
-                                             ]
-                                         },
-                                         {
-                                             "type": "section",
-                                             "fields": [
-                                                 {
-                                                     "type": "mrkdwn",
-                                                     "text": f"*Start Date:*\n{start_date.strftime('%A, %B, %d, %Y')}",
-                                                 },
-                                                 {
-                                                     "type": "mrkdwn",
-                                                     "text": f"*End Date:*\n{end_date.strftime('%A, %B, %d, %Y')}",
-                                                 }
-                                             ]
-                                         },
-                                         {
-                                             "type": "section",
-                                             "fields": [
-                                                 {
-                                                     "type": "mrkdwn",
-                                                     "text": f"*Reason:*\n{reason_of_leave}"
-                                                 }
-                                             ]
-                                         },
-
-                                     ])
+                                     blocks=confirm_requester_message_block)
 
     def approve_pto(self, ack, body, logger):
         self._process_pto_actions(body, ack)
