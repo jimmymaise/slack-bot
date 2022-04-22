b9382b5688d77f9dffdb7b242f432940756fc397
Jimmy Mai <jimmy.mai@bimodalconsulting.com>
2022-04-21 22:06:33 -0700

    update must-read feature to support edit, trigger when at the beginning only

--- application/handlers/bot/must_read_message.py
+++ application/handlers/bot/must_read_message.py
@@ -16,34 +16,72 @@ class MustReadMessage(BaseManagement):
     ):
         super().__init__(app, client)
         self.client = client
-        app.message('#must-read')(ack=self.respond_to_slack_within_3_seconds, lazy=[self.add_must_read_message_lazy])
+        app.event('message')(
+            ack=self.respond_to_slack_within_3_seconds,
+            lazy=[self.add_must_read_message_lazy],
+        )
+
         app.command('/must-read-not-completed')(
             ack=self.respond_to_slack_within_3_seconds,
             lazy=[self.get_my_must_read_messages_not_completed_read],
         )
 
     def add_must_read_message_lazy(self, body, context, say):
-        tagged_users = self.get_tagged_users_from_message(body['event'])
+        event_sub_type = body['event'].get('subtype')
+        is_edited_message = event_sub_type == 'message_changed'
+        if is_edited_message:
+            message_event = body['event']['message']
+            message = body['event']['message']['text']
+            message_ts = body['event']['message']['ts']
+            user_id = body['event']['message']['user']
 
-        normalize_content = re.sub(r"""<.+?>""", '', body['event']['text']).replace('\n', '').replace('#must-read', '')
-        short_content: str = normalize_content[0:100]
+        else:
+            message_event = body['event']
+            message = body['event']['text']
+            message_ts = body['event']['ts']
+            user_id = body['event']['user']
+
+        if '#must-read' not in message:
+            return
+        is_already_must_read = is_edited_message and bool(
+            self.must_read_db_handler.get_must_read_messages(
+                author_user_id=user_id,
+                message_ts=message_ts,
+            ),
+        )
+        if is_already_must_read:
+            return
+
         channel = body['event']['channel']
+        if not message.startswith('#must-read'):
+            return self.client.chat_postEphemeral(
+                channel=channel,
+                user=user_id,
+                text='You might try to create must read message but '
+                     'forgot to put #must-read at the beginning of message. '
+                     'Please edit it and create again if you want to do so',
+            )
+
+        tagged_users = self.get_tagged_users_from_message(message_event)
+
+        normalize_content = re.sub(r"""<.+?>""", '', message).replace('\n', '').replace('#must-read', '')
+        short_content: str = normalize_content[0:100]
 
         if not tagged_users:
             return self.client.chat_postEphemeral(
                 channel=channel,
-                user=context.user_id,
+                user=user_id,
                 text='You might try to create must read message but forgot to tag anyone.'
-                     'Please delete it and create again if you want to do so',
+                     'Please edit it and create again if you want to do so',
 
             )
 
-        permalink = self.app.client.chat_getPermalink(message_ts=body['event']['ts'], channel=channel)[
+        permalink = self.app.client.chat_getPermalink(message_ts=message_ts, channel=channel)[
             'permalink'
         ]
         self.must_read_db_handler.add_must_read_messages(
-            message_ts=f"ts_{body['event']['ts']}",
-            author_user_id=context.user_id,
+            message_ts=message_ts,
+            author_user_id=user_id,
             status=self.constant.MUST_READ_STATUS_IN_PROGRESS,
             short_content=short_content,
             permalink=permalink,
@@ -64,7 +102,7 @@ class MustReadMessage(BaseManagement):
             )
         return self.client.chat_postEphemeral(
             channel=channel,
-            user=context.user_id,
+            user=user_id,
             text='You just made a must read message and I have informed it '
                  f'to the following tagged users {",".join([f"<@{tagged_user}>" for tagged_user in tagged_users])}.'
                  f'You can type the command `/must-read-not-completed ` to check its status',
